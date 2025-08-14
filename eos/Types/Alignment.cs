891159bf52f2ad2ade73eb0c32bd92dc65d8f032
David <bouchard.dav@gmail.com>
2025-08-14 19:32:35 -0400

    Revert "Export: Allow alignments would error in the NWN toolset"
    This reverts commit ce0d316ee053da9075a0864d1f2147abe59ba977.

--- eos/Types/Alignment.cs
+++ eos/Types/Alignment.cs
@@ -56,30 +56,12 @@ namespace Eos.Types
         static Alignments()
         {
             alignmentLookup.Clear();
-            
-            // First pass: populate with non-inverted alignments
-            for (int flag = 0; flag <= 0x1F; flag++)
+            for (int flag=0; flag <= 0x1F; flag++)
             {
-                for (int axis = 0x00; axis <= 0x03; axis++)
+                for (int axis=0x00; axis <= 0x03; axis++)
                 {
-                    var alignment = Create(flag, axis, false);
-                    if (!alignmentLookup.ContainsKey(alignment))
-                    {
-                        alignmentLookup[alignment] = new AlignmentTuple(flag, axis, false);
-                    }
-                }
-            }
-            
-            // Second pass: populate with inverted alignments only if not already present
-            for (int flag = 0; flag <= 0x1F; flag++)
-            {
-                for (int axis = 0x00; axis <= 0x03; axis++)
-                {
-                    var alignment = Create(flag, axis, true);
-                    if (!alignmentLookup.ContainsKey(alignment))
-                    {
-                        alignmentLookup[alignment] = new AlignmentTuple(flag, axis, true);
-                    }
+                    alignmentLookup[Create(flag, axis, false)] = new AlignmentTuple(flag, axis, false);
+                    alignmentLookup[Create(flag, axis, true)] = new AlignmentTuple(flag, axis, true);
                 }
             }
         }
@@ -98,50 +80,40 @@ namespace Eos.Types
 
         public static Alignment Create(int flags, int axis, bool invert)
         {
-            bool isRestricted;
+            bool done;
             int nAlignRestrict = flags;
             int nAlignRestrictType = axis;
 
             Alignment result = (Alignment)0;
             foreach (var alignment in Enum.GetValues<Alignment>())
             {
-                isRestricted = false;
-                
-                // Check law-chaos axis restrictions
+                done = false;
                 if ((nAlignRestrictType & AXIS_LC) != 0)
                 {
                     if ((Alignments.Lawful.HasFlag(alignment) && ((nAlignRestrict & ALIGN_LAWFUL) != 0)) ||
                         (Alignments.NeutralLC.HasFlag(alignment) && ((nAlignRestrict & ALIGN_NEUTRAL) != 0)) ||
                         (Alignments.Chaotic.HasFlag(alignment) && ((nAlignRestrict & ALIGN_CHAOTIC) != 0)))
                     {
-                        isRestricted = true;
+                        if (invert)
+                            result |= alignment;
+                        done = true;
                     }
                 }
 
-                // Check good-evil axis restrictions
-                if ((nAlignRestrictType & AXIS_GE) != 0)
+                if (((nAlignRestrictType & AXIS_GE) != 0) && (!done))
                 {
                     if ((Alignments.Good.HasFlag(alignment) && ((nAlignRestrict & ALIGN_GOOD) != 0)) ||
                         (Alignments.NeutralGE.HasFlag(alignment) && ((nAlignRestrict & ALIGN_NEUTRAL) != 0)) ||
                         (Alignments.Evil.HasFlag(alignment) && ((nAlignRestrict & ALIGN_EVIL) != 0)))
                     {
-                        isRestricted = true;
+                        if (invert)
+                            result |= alignment;
+                        done = true;
                     }
                 }
 
-                // Add alignment to result based on restriction status and invert flag
-                if (invert)
-                {
-                    // When inverted, include only restricted alignments
-                    if (isRestricted)
-                        result |= alignment;
-                }
-                else
-                {
-                    // When not inverted, include only non-restricted alignments
-                    if (!isRestricted)
-                        result |= alignment;
-                }
+                if (!done && !invert)
+                    result |= alignment;
             }
 
             return result;
